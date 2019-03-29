a52cb1789521cf7b8dbbea8115e37a726f5b5403
Christian Shewmake <christian.a.shewmake@wustl.edu>
2019-03-29 12:43:39 -0500

    Simulate Dynamics

--- simulate.py
+++ simulate.py
@@ -2,15 +2,14 @@ import numpy as np
 import matplotlib
 matplotlib.interactive(True)
 import matplotlib.pyplot as plt
-
 from scipy.integrate import odeint
 
-x0 = [1,1]
-t = np.linspace(0,5,1000)
+x0 = [1,0]
+t = np.linspace(0,10,1000)
 
 def f(x,t):
-    return [x[0],-x[1]]
+    return [x[1],-x[0]]
 
 x_t = odeint(f, x0, t)
 
-plt.plot(t,x[:,0])
+plt.plot(x_t[:,0],x_t[:,1])
