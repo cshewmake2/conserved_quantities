4de0191aad2437f234b7f8eac2de839d73265d1c
Blake Bordelon <Blake@Blake-dell>
2019-03-29 15:00:39 -0500

    moving to sequential models

--- auto_encoder.py
+++ auto_encoder.py
@@ -2,34 +2,120 @@ import keras
 import numpy as np
 from keras.layers import Input, Dense
 from keras.models import Model
+from keras.models import Sequential
 from keras import regularizers
 
 
 def hi():
 	print("hi")
 
-def train_encoder(state_derivs, encoded_dim, num_layers, l1_penalty):
+# def train_encoder(state_derivs, encoded_dim, num_layers, l1_penalty):
+		
+# 	(num_data_pts, input_dim) = state_derivs.shape
+
+# 	print(input_dim)
+# 	input_layer = Input(shape=(input_dim,))
+
+# 	left_layers = []
+
+# 	for n in range(num_layers):
+# 		if n == 0:
+# 			encoded_layer_current = Dense(input_dim, activation = 'tanh')(input_layer)
+# 			left_layers.append(encoded_layer_current)
+# 		else:
+# 			encoded_layer_current = Dense(input_dim, activation = 'tanh')(left_layers[n-1])
+# 			left_layers.append(encoded_layer_current)
+
+
+# 	encoded_layer = []
+# 	if num_layers == 0:
+# 		encoded_layer = Dense(encoded_dim, activation = 'tanh', activity_regularizer=regularizers.l1(l1_penalty))(input_layer)
+# 	else:
+# 		encoded_layer = Dense(encoded_dim, activation = 'tanh', activity_regularizer=regularizers.l1(l1_penalty))(left_layers[len(left_layers)-1])
+
+
+# 	right_layers = []
+# 	for n in range(num_layers):
+# 		if n == 0:
+# 			encoded_layer_current = Dense(input_dim, activation = 'tanh')(input_layer)
+# 			right_layers.append(encoded_layer_current)
+# 		else:
+# 			encoded_layer_current = Dense(input_dim, activation = 'tanh')(right_layers[n-1])
+# 			right_layers.append(encoded_layer_current)	
+
+
+# 	decoded_layer = []
+# 	if num_layers == 0:
+# 		decoded_layer = Dense(input_dim, activation = 'linear')(encoded_layer)
+
+# 	else:
+# 		decoded_layer = Dense(input_dim, activation = 'linear')(right_layers[len(right_layers)-1])
+	
+
+
+# 	encoder = Model(input_layer, encoded_layer)
+
+# 	autoencoder = Model(input_layer, decoded_layer)
+
+# 	encoded_input = Input(shape=(encoded_dim,))
+# 	# retrieve the last layer of the autoencoder model
+# 	decoder_layer = autoencoder.layers[-1]
+# 	# create the decoder model
+	
+# 	if n==0:
+# 		decoder = Model(encoded_input, decoder_layer(encoded_input))
+# 	else:
+# 		decoder = Model(encoded_input, )
+
+# 	autoencoder.compile(optimizer = 'adadelta', loss = 'mean_squared_error')
+
+# 	print(state_derivs)
+
+# 	autoencoder.fit(state_derivs, state_derivs,
+#                 epochs=500,
+#                 batch_size=256,
+#                 shuffle=True)
+
+# 	return [autoencoder, encoder, decoder]
+
+
+
+
+def train_encoder(state_derivs, encoding_dim, num_layers, l1_penalty):
 		
 	(num_data_pts, input_dim) = state_derivs.shape
 
 	print(input_dim)
 	input_layer = Input(shape=(input_dim,))
 
-	encoded_layer = Dense(encoded_dim, activation = 'relu', activity_regularizer=regularizers.l1(l1_penalty))(input_layer)
+	autoencoder = Sequential()
 
-	decoded_layer = Dense(input_dim, activation = 'linear')(encoded_layer)
+	for n in range(num_layers):
+		if n==0:
 
-	
-	encoder = Model(input_layer, encoded_layer)
+			autoencoder.add( Dense(input_dim, input_shape=(input_dim,), activation='tanh') )
+		else:
+			autoencoder.add( Dense(input_dim, activation='tanh'))
 
-	autoencoder = Model(input_layer, decoded_layer)
+	if num_layers == 0:
+		autoencoder.add(    Dense(encoding_dim, input_shape=(input_dim,), activation='tanh') )
+	else:
+		autoencoder.add(  Dense(encoding_dim, activation='tanh') )
 
-	encoded_input = Input(shape=(encoded_dim,))
-	# retrieve the last layer of the autoencoder model
-	decoder_layer = autoencoder.layers[-1]
-	# create the decoder model
-	decoder = Model(encoded_input, decoder_layer(encoded_input))
+	for n in range(num_layers):
+		if n==0:
 
+			autoencoder.add( Dense(input_dim, input_shape=(input_dim,), activation='tanh') )
+		else:
+			autoencoder.add( Dense(input_dim, activation='tanh'))
+
+
+	encoding_layer = autoencoder.layers[num_layers]
+	encoder = Model(input)
+	decoder = Sequential()
+
+
+	autoencoder.add( Dense(input_dim, activation='linear') )
 
 	autoencoder.compile(optimizer = 'adadelta', loss = 'mean_squared_error')
 
--- simulate.py
+++ simulate.py
@@ -9,15 +9,25 @@ t = np.linspace(0,10,1000)
 def f(x,t):
     return [x[1],-x[0]]
 
+
 x_t = odeint(f, x0, t)
 
 
+x_dot = [f(x,0) for x in x_t]
 
-[autoencoder, encoder, decoder] = ae.train_encoder(x_t, 2 , 0 , )
+x_dot = np.array(x_dot)
 
-x_hat = autoencoder.predict(x_t)
+[autoencoder, encoder, decoder] = ae.train_encoder(x_dot, 1 , 2 , 0)
+
+x_hat = autoencoder.predict(x_dot)
+
+z_t = encoder.predict(x_dot)
 
 plt.plot(x_t[:,0],x_t[:,1])
 plt.plot(x_hat[:,0],x_hat[:,1])
+#plt.plot(z_t[:,0], z_t[:,1])
 plt.show()
 
+
+plt.plot(t, z_t)
+plt.show()
